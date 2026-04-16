325977f15a8e049e0b04ef7bb8b71ea5d805e019
Consigliere <lauragv2005@gmail.com>
2026-04-16 05:37:23 +0200

    Power plots, sweep plots. Pre meeting 16/04

---
+++ Codes/FWM_sweep_plots.m
@@ -0,0 +1,123 @@
+% ----------
+%% SETTINGS
+% ----------
+clear; close all; format long g
+s = settings;
+s.matlab.appearance.figure.GraphicsTheme.TemporaryValue = "light";
+set(groot, 'defaultTextInterpreter', 'latex')
+set(groot, 'defaultAxesTickLabelInterpreter','latex'); set(groot, 'defaultLegendInterpreter','latex');
+
+
+%-------------
+%%  LOAD DATA
+%-------------
+dataFolder = '../Tests/FWM_sweep_test_02';
+filePattern = fullfile(dataFolder, '*.csv');
+csvFiles = dir(filePattern);
+numFiles=length(csvFiles);
+
+sweepData = cell(numFiles, 1);
+
+for k = 1:numFiles
+    % Path for current file
+    baseFileName = csvFiles(k).name;
+    fullFileName = fullfile(csvFiles(k).folder, baseFileName);
+
+    % Load data
+    sweepData{k} = readmatrix(fullFileName);
+end
+
+% ------------
+%% SINGLE PLOT
+% ------------
+for i = [1, 11, 30, 50, 51, 60, 80, 90, 100]
+    sanityCheck = sweepData{i};
+    fig = figure(i);
+    plot(sanityCheck(:,1), sanityCheck(:,2), 'Color', '#000000');
+    xlabel('Wavelength'); ylabel('Power (dBm)');
+    title(sprintf('Sweep Data for File %d', i));
+    grid on;
+    outFolder = '../Tests/FWM_sweep_test_02';
+    if ~exist(outFolder, 'dir'), mkdir(outFolder); end
+    fname = fullfile(outFolder, sprintf('plot_single_%02d.png', i));
+    saveas(fig, fname);
+    i = i+1;
+end
+
+
+% ------------------------
+%% PEAK TRACKING PIPELINE
+% ------------------------
+
+% Physical constants
+c = 299792458;
+wl_pump = 1.54965e-06; % exp 1550.12nm, but checked graphs
+pump_window = 0.1e-9; %1.0e-9
+search_window = 0.5e-9;
+
+genPower = zeros(numFiles, 2);
+seedPower = zeros(numFiles, 2);
+
+for k = 1:numFiles
+    x = sweepData{k}(:,1);
+    y = sweepData{k}(:,2);
+
+    % Find seed position. We ignore pump region.
+    not_pump = (x < (wl_pump - pump_window)) | (x > (wl_pump + pump_window));
+    x_noPump = x(not_pump);
+    y_noPump = y(not_pump);
+
+    % Max oustide region = seed
+    [maxSeedPower, seedIdx] = max(y_noPump);
+    wl_seed = x_noPump(seedIdx);
+    seedPower(k, 1) = wl_seed; % Corresponding wavelength for seed
+    seedPower(k, 2) = maxSeedPower; % Max seed power
+
+    % Reject seed = pump
+    if abs(wl_seed - wl_pump) < 0.00112e-6%8.8e-10     % 8.8e-10 exact
+        genPower(k,1) = NaN;
+        genPower(k,2) = NaN;
+        continue;           % maybe include wvl?
+    end
+
+    % Calculate idler theoretical position
+    w_pump = c / wl_pump; w_seed = c / wl_seed;
+    w_idler = 2*w_pump - w_seed;
+    wl_idlerTheo = c / w_idler;
+
+    % Dynamic window arround theo value
+    int = (x >= (wl_idlerTheo - search_window)) & (x <= (wl_idlerTheo + search_window));
+    if any(int)
+        [maxY, relIdx] = max(y(int));
+        idx = find(int);
+        genPower(k, 1) = x(idx(relIdx)); % Corresponding wavelength for generated power
+        genPower(k, 2) = maxY; % Max generated power
+    else
+        genPower(k, 1) = NaN;
+        genPower(k, 2) = NaN;
+    end
+end
+genPower(11,:) = []; seedPower(11,:) = [];
+
+% -------------------
+%% FWM VISUALIZATION
+% -------------------
+wl_idler = genPower(:,1) * 1e9;
+
+conv_eff_dB = genPower(:,2) - seedPower(:,2);
+
+genPower_mW = 10.^(genPower(:,2) / 10);
+seedPower_mW = 10.^(seedPower(:,2) / 10);
+conv_eff_lin = genPower_mW ./ seedPower_mW;
+
+fig = figure('Name', 'FWM Conversion Efficiency dB');
+plot(wl_idler, conv_eff_dB, 'squarek', 'LineWidth', 1.5, 'MarkerFaceColor', 'k')
+%axis([1490 1610 -45 -20]); grid on
+xlabel('Generated photon wavelength (nm)'); grid on
+ylabel('FWM Conversion Efficiency (dB)')
+title('FWM Conversion Efficiency')
+
+outFolder = '../Tests/FWM_sweep_test_02';
+if ~exist(outFolder, 'dir'), mkdir(outFolder); end
+saveas(fig, fullfile(outFolder, 'FWM_conv_eff_dB.png'));
+%print(fig, fullfile(outFolder, 'FWM_conv_eff_dB.pdf'), '-dpdf');
---
+++ Codes/pump_sweep_plots.m
@@ -0,0 +1,123 @@
+% ----------
+%% SETTINGS
+% ----------
+clear; close all; format long g
+s = settings;
+s.matlab.appearance.figure.GraphicsTheme.TemporaryValue = "light";
+set(groot, 'defaultTextInterpreter', 'latex')
+set(groot, 'defaultAxesTickLabelInterpreter','latex'); set(groot, 'defaultLegendInterpreter','latex');
+
+%-------------
+%%  LOAD DATA
+%-------------
+dataFolder = '../Tests/Manual_Pump_Power_Sweep'; 
+filePattern = fullfile(dataFolder, '*.csv');
+csvFiles = dir(filePattern);
+numFiles = length(csvFiles);
+
+% 1. Constants del Sistema
+c = 299792458;                   
+wl_pump = 1549.65e-9; % La posició del Pump "vista" per l'OSA
+wl_seed = 1545.00e-9; % La posició fixa que hem configurat al Seed
+
+% Calcular on està l'Idler 
+f_pump = c / wl_pump;
+f_seed = c / wl_seed;
+f_idler = 2 * f_pump - f_seed;
+wl_idler = c / f_idler; 
+
+search_window = 0.5e-9; % Finestra de +/- 0.5 nm
+
+% Pre-assignació de memòria
+P_pump_dBm = zeros(numFiles, 1);
+P_idler_dBm = zeros(numFiles, 1);
+
+% --- EXTRACCIÓ DE DADES ---
+for k = 1:numFiles
+    baseFileName = csvFiles(k).name;
+    fullFileName = fullfile(csvFiles(k).folder, baseFileName);
+    data = readmatrix(fullFileName);
+    
+    x = data(:,1); % wvl
+    y = data(:,2); % power dBm
+    
+    % 1. Find pump power
+    idx_pump = (x >= (wl_pump - search_window)) & (x <= (wl_pump + search_window));
+    P_pump_dBm(k) = max(y(idx_pump));
+    
+    % 2. Find exp power of generated idle
+    idx_idler = (x >= (wl_idler - search_window)) & (x <= (wl_idler + search_window));
+    
+    % Failsafe per si el soroll tapa l'Idler a potències molt baixes
+    if any(idx_idler)
+        P_idler_dBm(k) = max(y(idx_idler));
+    else
+        P_idler_dBm(k) = NaN;
+    end
+end
+
+% Sort function: Ordenem els vectors respecte el Pump.
+[P_pump_dBm, sortIdx] = sort(P_pump_dBm);
+P_idler_dBm = P_idler_dBm(sortIdx);
+
+% Creem màscara de valors vàlids per no fer col·lapsar els 'polyfit'
+valid = ~isnan(P_pump_dBm) & ~isnan(P_idler_dBm);
+P_pump_dBm_val = P_pump_dBm(valid);
+P_idler_dBm_val = P_idler_dBm(valid);
+
+% --- CONVERSIÓ A LINEAL ---
+P_pump_mW_val = 10.^(P_pump_dBm_val / 10);
+P_idler_mW_val = 10.^(P_idler_dBm_val / 10);
+
+% --- VISUALITZACIÓ  ---
+fig = figure('Name', 'Idler Power vs Pump Power', 'Position', [100, 100, 900, 400]);
+sgtitle('Idler Power vs Pump Power')
+
+%% PLOT
+% --- SUBPLOT 1: Escala Lineal (mW) ---
+% Hauries de veure una paràbola (P_idler depèn del quadrat de P_pump)
+subplot(1, 2, 1);
+plot(P_pump_mW_val, P_idler_mW_val, 'squarek', 'LineWidth', 1.5, 'MarkerFaceColor', '#000000');
+grid on;
+xlabel('Pump Power (mW)');
+ylabel('Idler Power (mW)');
+title('Linear scale (mW)')
+
+% Fit Quadràtic (Grau 2)
+p_lin = polyfit(P_pump_mW_val, P_idler_mW_val, 2);
+% Generem una x fina per a que la corba es vegi suau
+x_fit_lin = linspace(min(P_pump_mW_val), max(P_pump_mW_val), 100);
+yfit_lin = polyval(p_lin, x_fit_lin);
+hold on; 
+plot(x_fit_lin, yfit_lin, '-', 'LineWidth', 1, 'Color', '#a9a9a9'); 
+hold off;
+
+% --- SUBPLOT 2: Escala Logarítmica (dBm) ---
+% Hauries de veure una línia recta amb pendent ≈ 2
+subplot(1, 2, 2);
+plot(P_pump_dBm_val, P_idler_dBm_val, 'pentagramk', 'LineWidth', 1.5, 'MarkerFaceColor', '#000000');
+grid on;
+xlabel('Pump Power (dBm)');
+ylabel('Idler Power (dBm)');
+title('Logarithmic scale (dBm)');
+
+% Fit Lineal (Grau 1)
+p_log = polyfit(P_pump_dBm_val, P_idler_dBm_val, 1);
+yfit_log = polyval(p_log, P_pump_dBm_val);
+hold on; 
+plot(P_pump_dBm_val, yfit_log, '-', 'LineWidth', 1, 'Color', '#a9a9a9'); 
+
+% Add slope label
+slope_val = p_log(1);
+txt = sprintf('m = %.3g dB/dB', slope_val);
+% Ajust automàtic de la posició del text per evitar hardcoding que el deixi fora del gràfic
+xpos = min(P_pump_dBm_val) + (max(P_pump_dBm_val)-min(P_pump_dBm_val))*0.1;
+ypos = max(P_idler_dBm_val) - (max(P_idler_dBm_val)-min(P_idler_dBm_val))*0.1;
+t = text(xpos, ypos, txt, 'BackgroundColor', 'white', 'EdgeColor', 'k');
+uistack(t, 'top');
+hold off;
+
+% Guardar la figura
+outFolder = dataFolder;
+if ~exist(outFolder, 'dir'), mkdir(outFolder); end
+saveas(fig, fullfile(outFolder, 'Pump_power_sweep.png'));
